65480f3cc0af2c748e6641e4160b9e22e8784191
Hammad Toufeeq <hammadtoufeeq02@gmail.com>
2026-09-21 14:58:51 +0500

    port changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    port: 3000  
+  }
 })
